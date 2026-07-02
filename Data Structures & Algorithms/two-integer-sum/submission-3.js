9e653fe738290cc263ccfc1044c20f5ed953ed66
Marcus Vinicius Ximenes de Souza <marcusvx@users.noreply.github.com>
2026-07-01 23:55:19 -0300

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const numsMap = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if (numsMap.has(diff)) {
+                return [i, numsMap.get(diff)];
+            }
+
+            numsMap.set(nums[i], i);
+        }
+    }
+}
